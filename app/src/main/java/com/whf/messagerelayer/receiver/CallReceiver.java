99f783ef65a12c25cf64283d6d4d58b54063474b
vissong <slxvista@qq.com>
2018-05-23 00:35:07 +0800

    增加未接来电提醒，merge from rjbule branch

---
+++ app/src/main/java/com/whf/messagerelayer/receiver/CallReceiver.java
@@ -0,0 +1,27 @@
+package com.whf.messagerelayer.receiver;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.provider.CallLog;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+
+import com.whf.messagerelayer.utils.NativeDataManager;
+
+/**
+ * Created by lingxuan on 2017/7/27.
+ */
+public class CallReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Service.TELEPHONY_SERVICE);
+        if (tm.getCallState() == TelephonyManager.CALL_STATE_RINGING) {
+            Log.d("sms", "CALL_STATE_RINGING register content observer");
+            context.getContentResolver().registerContentObserver(CallLog.Calls.CONTENT_URI, false, new CallObserver(context, new Handler()));
+        }
+    }
+}
